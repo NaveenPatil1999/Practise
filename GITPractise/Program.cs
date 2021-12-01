09e5847a6005ad62b354632c9c05d43568e6b832
Naveen <naveenpatil@gmail.com>
2021-12-01 16:56:30 +0530

    Welcome Ajit

---
+++ GITPractise/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GITPractise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Collaborator ")
+        }
+    }
+}
